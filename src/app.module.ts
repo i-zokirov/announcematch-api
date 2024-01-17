bd938f424098be784c2df20c6f5fb1d328e6604e
Anorsoft <izokirov@outlook.com>
2024-01-17 21:43:50 +0500

    generated required modules for the projects and installed required dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { ProposalsModule } from './proposals/proposals.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { CategoriesModule } from './categories/categories.module';
+import { MessagesModule } from './messages/messages.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, AnnouncementsModule, ProposalsModule, NotificationsModule, MessagesModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
